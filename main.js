75e4c30b13aaa357e5e8d27523aa8213130c5dca
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2025-05-20 14:36:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -30,17 +30,16 @@ switch (name_) {
     case "楊濟安":
         document.querySelector(".card-body").innerHTML = `
             畢業快樂！<br>            
-            是說你到底什麼時候畢業<br>
-            總之感謝你今年當隊長為球隊想了這麼多<br>
+            感謝你今年當隊長為球隊想了這麼多<br>
             相信下一任隊長會延續你的改革<br>
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;
     case "簡國盛":
         document.querySelector(".card-body").innerHTML = `
             畢業快樂！<br>            
             謝謝你在我打球感到挫折的時候會來關心我 給我建議<br>
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;
     case "江巧羽":
@@ -48,47 +47,46 @@ switch (name_) {
             畢業快樂！<br>            
             謝謝你偶爾會教我打球<br>
             也謝謝你提供你家客廳給我們煮火鍋<br>
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;  
     case "莊采柔":
         document.querySelector(".card-body").innerHTML = `
             畢業快樂！<br>            
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;
     case "林鈺芳":
         document.querySelector(".card-body").innerHTML = `
             畢業快樂！<br>            
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;
     case "廖國艮":
         document.querySelector(".card-body").innerHTML = `
             畢業快樂！<br>
             謝謝你出去比賽的時候帶我去吃好吃的 <br>
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;
     case "張鈞棋":
         document.querySelector(".card-body").innerHTML = `
             畢業快樂！<br>            
-            祝事事順心！<br>
+            祝你心想事成！<br>
         `;
         break;
     case "王邦綸":
         document.querySelector(".card-body").innerHTML = `
             你要畢業了?!<br>
             有空再回來打球<br>
-            畢業快樂 祝事事順心！<br>
+            祝你畢業快樂 心想事成!<br>
         `;
         break;
     case "鄧韋辰":
         document.querySelector(".card-body").innerHTML = `
             謝謝你開車載我們去露營<br>
             也謝謝你跟我說我海洋被點到名<br>
-            沒有你我真的會被當<br>
-            畢業快樂 祝事事順心！<br>
+            祝你畢業快樂 心想事成!<br>
         `;
         break;
     case "王紀堯":
